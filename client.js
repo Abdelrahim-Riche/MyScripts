52e461a6aea7cb60c1912388d9be921e5040382e
Abdelrahim-Riche <richeabdelrahim.dev@gmail.com>
2024-07-25 20:28:51 +0200

    reset

--- client.js
+++
@@ -1,7 +0,0 @@
-// Écouter l'événement de réapparition du joueur
-on('playerSpawned', () => {
-    emitNet('spawnPlayer');
-});
-
-// Importer les scripts de gestion des commandes
-require('./commands/getcoords.js');
--- commands/getcoords.js
+++
@@ -1,6 +0,0 @@
-// Commande pour obtenir les coordonnées actuelles
-RegisterCommand('getcoords', () => {
-    const playerPed = PlayerPedId();
-    const [x, y, z] = GetEntityCoords(playerPed, true);
-    console.log(`Current coordinates: X: ${x}, Y: ${y}, Z: ${z}`);
-}, false);
